4b20cc513d69e493d268f8ca804de1f5765ff27b
jsrc <jsrc001@foxmail.com>
2016-07-25 18:01:10 +0800

    增加项目

---
+++ njtrtest/njtrtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  njtrtest
+//
+//  Created by 江晨 on 15-5-28.
+//  Copyright (c) 2015年 JC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
