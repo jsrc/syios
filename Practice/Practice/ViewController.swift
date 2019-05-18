eb39abaacb00a37488bdf7c0d80610fc52e27041
jsrc <onriver@outlook.com>
2019-05-18 13:14:16 +1000

    init

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by 江上 on 2019/5/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
